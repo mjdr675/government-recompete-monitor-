c6bfcd9bd5a542e26e5942daeeb4e209c2783412
mjdr675 <mjdr675@gmail.com>
2026-06-17 15:06:57 -0400

    Update janitorial_recompete_report.py

--- janitorial_recompete_report.py
+++ janitorial_recompete_report.py
@@ -7,11 +7,6 @@ from datetime import date, timedelta
 USASPENDING_URL = "https://api.usaspending.gov/api/v2/search/spending_by_award/"
 NAICS = "561720"  # Janitorial Services
 
-import json
-
-contracts = fetch_expiring_contracts()
-
-print(json.dumps(contracts[0], indent=2))
 
 def fetch_expiring_contracts(days_ahead=540, limit=100):
     today = date.today()
@@ -111,4 +106,7 @@ def build_report():
 
 
 if __name__ == "__main__":
-    build_report()
+    import json
+
+    contracts = fetch_expiring_contracts()
+    print(json.dumps(contracts[0], indent=2))
